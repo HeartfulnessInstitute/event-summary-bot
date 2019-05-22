f7491ea1214d4a510d2cb310d1eef3e09ec7825b
Krishna Sai <admin@Krishnas-MacBook-Pro.local>
2019-05-22 19:40:47 +0530

    Added support for Group meditations/Satsanghs, and used slot filling in the callback to check if we need to promot for which day. Also updated the agent to enable slot filling and make event_day as an optional parameter

--- functions/index.js
+++ functions/index.js
@@ -22,7 +22,6 @@ const {WebhookClient} = require('dialogflow-fulfillment');
 const {BigQuery} = require("@google-cloud/bigquery");
 const Fuse = require("fuse.js");
 const Cities = require("hfn-centers");
-const uuidv4 = require('uuid/v4');
 
 process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
 admin.initializeApp(functions.config().firebase);
@@ -108,53 +107,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
       return(dateOnly);
     }
 
-    function connectContact(event_type){
-        let connectContacts = {};
-		
-      	if (event_type) {
-          connectContacts["ROCF"] = "info@rocf.org";
-          connectContacts["S-Connect-HELP"] = "sconnect@heartfulness.org";
-          connectContacts["u-connect"] = "uconnect@heartfulness.org";
-          connectContacts["Family-Connect"] = "fconnect@heartfulness.org";
-          connectContacts["g-connect"] = "gconnect@heartfulness.org";
-          connectContacts["Divya-Janani"] = "divyajanani@heartfulness.org";
-          connectContacts["Research"] = "research@heartfulness.org";
-          connectContacts["Heartfulness-green"] = "green@heartfulness.org";
-          connectContacts["v-connect"] = "vconnect@heartfulness.org";
-          connectContacts["L-Connect"] = "lconnect@heartfulness.org";
-          connectContacts["NGO-Connect"] = "ngoconnect@heartfulness.org";
-          connectContacts["Yoga"] = "yoga@heartfulness.org";
-          connectContacts["glow-pearl"] = "webinars@heartfulness.org";
-          connectContacts["Brighter-Minds"] = "brighterminds@heartfulness.org";
-          connectContacts["at-work"] = "atwork@heartfulness.org";
-          connectContacts["S-Connect-HEART"] = "sconnect@heartfulness.org";
-          connectContacts["dhyanotsav"] = "dhyanotsav@heartfulness.org";
-          connectContacts["s-connect"] = "sconnect@heartfulness.org";
-          connectContacts["S-Connect-THWC"] = "sconnect@heartfulness.org";
-          connectContacts["books-and-more"] = "booksandmore@heartfulness.org";
-          connectContacts["S-Connect-INSPIRE"] = "sconnect@heartfulness.org";
-          connectContacts["Kaushalam"] = "kaushalam@heartfulness.org";
-          
-		  // Not supported currently
-          //connectContacts["Religious Institutions"] = "it@heartfulness.org";
-          //connectContacts["Heartfulness Introduction"] = "it@heartfulness.org";
-          //connectContacts["Other"] = "it@heartfulness.org";
-          //connectContacts["Youth"] = "it@heartfulness.org";
-          //connectContacts["group-meditation"] = "it@heartfulness.org";
-          //connectContacts["Indian Diaspora"] = "it@heartfulness.org";
-
-          if(event_type in connectContacts) {
-              return(connectContacts[event_type]);
-          }
-        }
-        return("");
-    }
-
     function welcomeMessage(agent) {
         agent.add(`Greetings! What Heartfulness event are you reporting on?
-        \nFor example, you can enter Dhyanotsav, U-Connect, AtWork, C-Connect, V-Connect, G-Connect, Kaushalam, CME, Youth, Yoga, Temple, Legal, Family, NGO, Brighter Minds, etc. 
-        \nFor general Heartfulness Introductory Events, enter "Heartfulness". 
-        \nFor *School or S-Connect* events, enter which program: HELP, INSPIRE, HEART or THWC
+        \nFor example, you can enter Dhyanotsav, U-Connect, AtWork, C-Connect, V-Connect, G-Connect, CME, Youth, Yoga, Temple, Legal, Family, NGO, Brighter Minds, etc. 
+        \nFor general Heartfulness Introductory Events, just enter "Heartfulness". 
+        \nFor School or S-Connect events, enter which program: HELP, INSPIRE, HEART or THWC
+        \nFor Group Meditations, simply enter "Satsangh" or "Group Meditation"
         \nIf you don't know just enter 'Other'.`);
         return;
 
@@ -162,16 +120,39 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
     function askForConfirmation(agent) {
         let type = agent.parameters['event_type'];
+        let event_day = agent.parameters['event_day'];
         let count = agent.parameters['event_count'];
         let name = agent.parameters['coordinator_name'];
+        let phone = agent.parameters['coordinator_phone'];
         let isoDateString = agent.parameters['event_date'];
         let institution = agent.parameters['event_institution'];
-        let center = findCity(agent.parameters['event_city']);
-        let date = cleanDate(isoDateString);
-        console.log(center.city, center.zone, center.country);
-        agent.add(`Okay, ${count} attended ${type} on ${date} at ${institution} in ${center.city}, Is this correct ${name}? Please reply with 'yes' or 'no'.`);
-        return;
+        let city = agent.parameters['event_city'];
+        let feedback = agent.parameters['event_feedback'];
 
+        const context = agent.getContext('event-info');
+
+		if(type) {
+          	console.log(type, event_day);
+	      	if (type != "group-meditation" && !event_day){
+              	console.log("Need which day");
+				agent.add(`Is is day-1, day-2 or day-3 of the event? If it is a one day event just enter \"one day event\". For a follow up event, enter \"Follow Up\"`);			
+            }
+        }
+
+        let allParametersReady = false;
+      	// Check if we have all required params
+        if (type && count && name && phone && isoDateString && institution &&  city && feedback) {
+	        allParametersReady = true;
+            console.log("Got all required params");
+        }
+
+        if(allParametersReady){
+            let center = findCity(city);
+            let date = cleanDate(isoDateString);
+            console.log(center.city, center.zone, center.country);
+            agent.add(`Okay, ${count} attended ${type} on ${date} at ${institution} in ${center.city}, Is this correct ${name}? Please reply with 'yes' or 'no'.`);
+        }
+        return;
     }
   
     function writeToDb (agent) {
@@ -197,9 +178,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         	source = "Unknown";
             source_data = "Maybe DialogFlow Console";
         }
-        let evuuid = uuidv4();
         let eventSummary = {
-            "id": evuuid,
             "name": name,
           	"phone": phone,
             "type": type,
@@ -221,19 +200,10 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             writeToBq(eventSummary);
             return Promise.resolve('Write complete');
         }).then(doc => {
-            let finalResponse = "Thanks for submitting the information and all the best.\n\n";
-            finalResponse += "Please submit the complete feedback with attendee information (if available) at our Events Portal: events.heartfulness.org\n\n";
-            finalResponse += "You can view the latest reports on Heartfulness Connect activities here: http://bit.ly/hfn-connect-report\n\n";
-            finalResponse += "If you like this app, please inform other coordinators to use the app by sending the following WhatsApp message to +14155238886:\
-                                \njoin harlequin-tuatara\n\n";
-            finalResponse += "Or if you prefer Telegram, start a chat with @hfn_event_bot to use this app\n\n";
-            if(connectContact(type) != "") {
-                finalResponse += "Please contact " + connectContact(type) + " for any questions or to send photos of the event.\n\n";
-            }
-            finalResponse += "For any help or feedback on this application, please email it@heartfulness.org."
-
-            agent.add(finalResponse);
-            
+            agent.add(`Thanks for submitting the information and all the best.\
+             \n\nPlease submit the complete feedback with attendee information (if available, for events) at our Events Portal: events.heartfulness.org\
+             \n\nIf you like this app please inform other coordinators to use the app by sending a *WhatsApp message to +14155238886* with code *join harlequin-tuatara*\
+             \n\nOr if you prefer *Telegram*, start a chat with @hfn_event_bot to use this app`);
         }).catch(err => {
             console.log(`Error writing to Firestore: ${err}`);
             agent.add(`Looks like we had some problem capturing this information. This could be due to some internal error. Can you please email itsupport@heartfulness.org with the screenshot? Thanks and apologies for the inconvenience.`);
